0f777a6c58b3c576f39e0059867d65035a711e69
Jakub Danelski <113716902+JakDan03@users.noreply.github.com>
2024-03-25 18:51:05 +0100

    Update and rename kółko i krzyżyk.py to tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# mechanizmy sprawdzające
+# definitions of winning and forking (used by a bot)
 def if_win(board, moves_available, sign):
     for i in range(len(moves_available)):
         if [board[moves_available[i][0]][j] for j in range(3) if j != moves_available[i][1]] == [sign for i in range(2)] or [board[j][moves_available[i][1]] for j in range(3) if j != moves_available[i][0]] == [sign for i in range(2)]:
@@ -31,23 +31,23 @@ def if_fork(board, moves_available, sign):
         if checks > 1: return moves_available[i]
     return False
 
-# jak wybiera bot?
+# how our bot is making a choice?
 def bot_choice(board, bot_sign, player_sign, move):
     
-    # wszystkie możliwe ruchy w danym posunięciu
+    # all available moves
     moves_available = [(i, j) for i in range(3) for j in range(3) if board[i][j] == " "]
     
-    # pierwszy ruch (środek albo róg)
+    # first move - middle (60% chance) or corner (40% chance)
     if move == 1:
-        if np.random.choice([True, False], p=[.4, .6]): return (1, 1)
+        if np.random.choice([True, False], p=[.6, .4]): return (1, 1)
         else: return (np.random.choice([0, 2]), np.random.choice([0, 2]))
     
-    # drugi ruch - jeśli się da to zawsze na środek, jeśli nie to zawsze w róg
+    # second move - if it's possible - move to a middle, if not - always to a corner
     elif move == 2:
         if (1, 1) in moves_available: return (1, 1)
         else: return (np.random.choice([0, 2]), np.random.choice([0, 2]))
     
-    # trzeci ruch - mamy szansę wygrać jeśli my środek i przeciwnik bok albo jeśli my róg i przeciwnik nie środek
+    # third move - bot wins if bot middle and opponent side or if bot corner and opponent not middle
     elif move == 3:
         moves_unavailable = [(i, j, board[i][j]) for i in range(3) for j in range(3) if (i, j) not in moves_available]
         bot_move = moves_unavailable[0] if moves_unavailable[0][2] == bot_sign else moves_unavailable[1]
@@ -69,29 +69,29 @@ def bot_choice(board, bot_sign, player_sign, move):
                 winning_moves = [winning_moves[i] for i in range(len(winning_moves)) if (winning_moves[i][0] == bot_move[0]) or (winning_moves[i][1] == bot_move[1])]
             return winning_moves[np.random.randint(len(winning_moves))]
         
-        # jeśli gracz dobrze się obronił przed rogiem to w 40% przypadków postaw w rogu tak, by zagrozić podwójnym forkiem
+        # if player defend correctly against a corner, 40% of the time corner which may end up in a double fork for a bot
         elif np.abs(bot_move[0] - bot_move[1]) != 1 and (player_move[0], player_move[1]) == (1, 1):
             if np.random.choice([True, False], p=[.4, .6]): return (np.abs(bot_move[0]-2),np.abs(bot_move[1]-2))
     
-    # ostatni ruch - rusz się na jedyne dostępne pole
+    # last move - play the only available one
     elif move == 9: return moves_available[0]
     
-    # w pozostałych ruchach postępuj zgodnie z zasadami (hierarchią) Newella i Simona
+    # else - follow Newell and Simon strategy
     else:
         
-        # sprawdź czy nie masz wygranej
+        # check if you can win
         res = if_win(board, moves_available, bot_sign)
         if res: return res
         
-        # sprawdź czy przeciwnik nie ma wygranej
+        # chech if the opponent can win and if he can - block him
         res = if_win(board, moves_available, player_sign)
         if res: return res
                 
-        # sprawdź czy nie masz forka - ruch, którym atakujesz z dwóch stron
+        # chech if bot has a fork - a double attack
         res = if_fork(board, moves_available, bot_sign)
         if res: return res
         
-        # jeśli gracz zagraża podwójnym forkiem w przypadku, gdzie gracz zaczął z rogam, to postaw z boku
+        # if the opponent may double fork (after his first corner move), bot has to play side
         if move==4:
             main_diagonal = [board[j][j] for j in range(3)]
             opp_diagonal = [board[j][2-j] for j in range(3)]
@@ -99,85 +99,87 @@ def bot_choice(board, bot_sign, player_sign, move):
                 winning_moves = [(0, 1), (1, 0), (1, 2), (2, 1)]
                 return winning_moves[np.random.randint(len(winning_moves))]
         
-        # sprawdź czy przeciwnik nie ma forka
+        # check if player has a fork
         res = if_fork(board, moves_available, player_sign)
         if res: return res
         
-        # jeśli gracz pierwsze dwa ruchy zrobił na przeciwległych bokach to wygrywasz stawiając na rogu
+        # if player make first two moves on facing sides, bot plays a corner and win
         if move==4:
             row_shapes = [board[1][j] for j in range(3)]
             col_shapes = [board[j][1] for j in range(3)]
             if row_shapes == [player_sign, bot_sign, player_sign] or col_shapes == [player_sign, bot_sign, player_sign]:
                 return (np.random.choice([0, 2]), np.random.choice([0, 2]))
             
-    # jeśli nie masz nic w zanadrzu to rusz się losowo
+    # in any other cases - move randomly
     return moves_available[np.random.randint(len(moves_available))]
 
-# mechanimz gry kółko i krzyżyk
+# tic tac toe mechanism
 def tictactoe(nick1, nick2, bot):
-    # nazwy graczy
+    
+    # players' names
     nicks = [nick1, nick2]
-    gracz1_shape = ""
-    while gracz1_shape != "o" and gracz1_shape != "x":
-        gracz1_shape = str(input(nick1 + ", wybierz kształt - prowadź 'o' lub 'x': "))
-    gracz2_shape = "x" if gracz1_shape == "o" else "o"
+    player1_shape = ""
+    while player1_shape != "o" and player1_shape != "x":
+        player1_shape = str(input(nick1 + ", choose player one shape - insert 'o' or 'x': "))
+    player2_shape = "x" if player1_shape == "o" else "o"
 
-    # kto zaczyna?
-    los = np.random.randint(1, 3)
+    # who starts?
+    rand = np.random.randint(1, 3)
 
-    # plansza
+    # board
     board = [[' ' for i in range(3)] for j in range(3)]
 
-    # funkcja drukująca planszę
+    # function printing a board
     def print_board():
         for i in range(5):
             print(" " + board[int(i/2)][0] + " |" + " " + board[int(i/2)][1] + " |" + " " + board[int(i/2)][2] + " ") if i%2==0 else print(11*"–")
 
-    # rozpoczęcie gry
-    print("Rozpoczyna " + nicks[los-1] + "!")
+    # start of the game
+    print(nicks[rand-1] + " begins!")
 
-    # rozgrywka
+    # gameplay
     turn = 1
     while turn <= 9:
-        if turn != 1: print(nicks[los-1] + " - twoja kolej!")
+        if turn != 1: print(nicks[rand-1] + " - your turn!")
         print_board()
-        if bot and nicks[los-1] == "bot":
-            row, col = bot_choice(board, gracz2_shape, gracz1_shape, turn)
+        if bot and nicks[rand-1] == "bot":
+            row, col = bot_choice(board, player2_shape, player1_shape, turn)
         else:
             row, col = -1, -1
             while row not in [0, 1, 2] or col not in [0, 1, 2]:
-                row = int(input("Wiersz: "))-1
-                col = int(input("Kolumna: "))-1
+                row = int(input("Row: "))-1
+                col = int(input("Column: "))-1
         if board[row][col] != " ": 
-            print("To pole jest już zajęte!")
+            print("Choose another place!")
             continue
-        board[row][col] = gracz1_shape if los == 1 else gracz2_shape
+        board[row][col] = player1_shape if rand == 1 else player2_shape
         if turn >= 5:
             if [board[row][i] for i in range(3)] == [board[row][col] for i in range(3)] or [board[i][col] for i in range(3)] == [board[row][col] for i in range(3)]:
-                print("Wygrywa " + nicks[los-1] + "!")
+                print(nicks[rand-1] + " wins!")
                 print_board()
                 break
             if row==col or np.abs(row-col) == 2:
                 if [board[i][i] for i in range(3)] == [board[row][col] for i in range(3)] or [board[i][2-i] for i in range(3)] == [board[row][col] for i in range(3)]:
-                    print("Wygrywa " + nicks[los-1] + "!")
+                    print(nicks[rand-1] + " wins!")
                     print_board()
                     break
             if turn == 9:
-                print("Remis!")
+                print("Tie!")
                 print_board()
         turn+=1
-        los = 1+los%2
+        rand = 1+rand%2
 
-# start gry
-print("Witaj w grze kółko i krzyżyk! Jeśli chcesz zagrać ze znajomym napisz 'friendly', a jeśli chcesz zagrać z botem napisz 'bot'")
-print("Niemniej i tak nie jesteś w stanie go ograć:p")
+# game activator
+print("Welcome to tic tac toe game! If you want to play with a friend, type 'friendly' and if you want to try a bot, write 'bot'")
+print("But it's actually impossible to beat him!")
+print("Unless...")
 mode = ""
 while mode != "friendly" and mode != "bot":
-    mode = str(input("Tryb gry: "))
+    mode = str(input("Choose a gamemode: "))
 if mode == "bot":
-    gracz_nick = str(input("Podaj swój nick: "))
-    tictactoe(gracz_nick, "bot", bot=True)
+    player_nick = str(input("Enter your name: "))
+    tictactoe(player_nick, "bot", bot=True)
 else:
-    gracz1 = str(input("Podaj nick gracza 1: "))
-    gracz2 = str(input("Podaj nick gracza 2: "))
-    tictactoe(gracz1, gracz2, bot=False)
+    player1 = str(input("Enter player 1 name: "))
+    player2 = str(input("Enter player 2 name: "))
+    tictactoe(player1, player2, bot=False)
